1e77f3e5c23d5ae6822f171c33c5e1e76c79a5cc
Grant Miller <grant@ievfx.com>
2015-03-01 18:15:49 -0800

    need lodash, need to know port

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 var app = require('express')()
 var http = require('http').Server(app)
+var os = require('os')
+var _ = require('lodash')
+
+
+var port = 3000
 
 app.get('/', function(req, res)
 {
 	res.sendFile('test10.zip', {root: __dirname})
 })
 
-http.listen(3000, function()
+http.listen(port, function()
 {
-	console.log('listening on *:3000')
+
+	console.log('\n\nServer listening at:\n')
+
+	// loop through the networkInterfaces and list
+	// each IPv4 that we find
+	_.each(os.networkInterfaces(), function(i)
+	{
+		_.each(i, function(entry)
+		{
+			if (entry.family == 'IPv4')
+				console.log('http://' +
+					entry.address + ':' +
+					port)
+		})
+	})
 })
