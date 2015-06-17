7279ce82974c2029d038c128cedebe108a6d8c4e
jonathanadam <jonathan_adam@brown.edu>
2015-06-17 14:42:05 -0700

    some undos

--- index.js
+++ index.js
@@ -3,106 +3,23 @@ var http = require('http').Server(app)
 var os = require('os')
 var _ = require('lodash')
 
-// Passport Boilerplate
-passport.serializeUser(function(user, done)
-{
-	done(null, user)
-})
 
-passport.deserializeUser(function(obj, done)
-{
-	done(null, obj)
-})
+var port = 3000
+var filename = '10mb.zip'
+var IPs = ''
 
-//Strategy setup: Get clientID, clientSecret and callbackURL from Google Developer Console
-passport.use(new GoogleStrategy
-	({
-	clientID        : GOOGLE_CLIENT_ID,
-	clientSecret    : GOOGLE_CLIENT_SECRET,
-	callbackURL     : GOOGLE_CALLBACK_URL
-	},
-	function(token, refreshToken, profile, done)
-	{
-		name = profile.displayName
-		//make the code asynchronous
-		// User.findOne won't fire until we have all our data back from Google
-		process.nextTick(function() {
-			//In a normal app here is where the app looks up user in database
-			if (profile.id == "112209607950970881138")
-			{
-				name = profile.displayName
-				id = profile.id
-				return done(null, profile)
-			}
-			else
-				return done(null, false)
-
-		})
-	})
-)
-
-//Configure express and its sessions
-app.use(session(
-	{secret: 'thisisasecret',
-	 resave: false,
-	 saveUninitialized: false
-	}))
-app.use(cookieParser())
-app.use(passport.initialize())
-app.use(passport.session())
-
-function isLoggedIn(req, res, next) {
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next()
-	// if they aren't redirect them to the home page
-	res.redirect('/')
+var serverInfo = {
+	'Type': 'type',
+	'Platform': 'platform',
+	'CPU Architecture': 'arch',
+	'Release': 'release',
+	'Uptime': 'uptime',
+	'Load Averages': 'loadavg',
+	'Total Memory': 'totalmem',
+	'Free Memory': 'freemem',
+	'CPUs': 'cpus',
+	'Network Interfaces': 'networkInterfaces'
 }
-
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
-app.get('/auth/google',
-	passport.authenticate('google', { scope: ['profile', 'email'] }),
-	function(req, res)
-	{
-	// The request will be redirected to Google for authentication, so this
-	// function will not be called.
-})
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get(CALLBACK_PATH,
-	passport.authenticate('google', { failureRedirect: '/fail' }),
-	function(req, res)
-	{
-	res.redirect('/success')
-	}
-)
-
-app.get('/fail', function(req,res)
-{
-	res.send('login failed')
-})
-
-app.get('/login',
-	passport.authenticate('google', {scope: ['profile', 'email']}),
-	function(req, res) {
-	res.redirect('/')
-});
-
-app.get('/logout', function(req, res){
-	name = ''
-	id = ''
-	req.logout()
-	res.redirect('/')
-});
->>>>>>> origin/master
 
 app.get('/file', function(req, res)
 {
@@ -110,18 +27,15 @@ app.get('/file', function(req, res)
 	res.set({
 		'Content-Disposition': 'attachment; filename="'+ filename +'"',
 		'Content-Type': 'application/zip'
-	})
+	});
 })
-
-
 
 app.get('/', function(req, res)
 {
 	var html = '<html><body style="font-family:monospace">' +
-		'<h1>Server Info</h1>' +
-		'<ul>'
-
-	html +='<li><strong>IPs:</strong><ul>' + IPs + '</ul></li>'
+		'<h1>Server Info: ' + os.hostname() + '</h1>' +
+		'<ul>' +
+		'<li><strong>IPs:</strong><ul>' + IPs + '</ul></li>'
 
 	var info
 	_.each(serverInfo, function(val, key)
